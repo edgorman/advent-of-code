54e65c053317a3a595828c4baebac3f02aca924c
Edward Gorman <ejgorman@gmail.com>
2020-12-07 18:10:57 +0000

    Day 2 solution

---
+++ 2020/Day 2/main.py
@@ -0,0 +1,69 @@
+"""
+--- Day 2: Password Philosophy ---
+Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
+
+The shopkeeper at the North Pole Toboggan Rental Shop is having a bad day. "Something's wrong with our computers; we can't log in!" You ask if you can take a look.
+
+Their password database seems to be a little corrupted: some of the passwords wouldn't have been allowed by the Official Toboggan Corporate Policy that was in effect when they were chosen.
+
+To try to debug the problem, they have created a list (your puzzle input) of passwords (according to the corrupted database) and the corporate policy when that password was set.
+
+For example, suppose you have the following list:
+
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+
+In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
+
+How many passwords are valid according to their policies?
+"""
+
+import os
+
+def part_one(entries):
+    valid_count = 0
+
+    # For each entry
+    for e in entries:
+        # Check if valid
+        if e[1] >= e[3].count(e[2]) >= e[0]:
+            # Increment valid count
+            valid_count = valid_count + 1
+    
+    return valid_count
+
+def part_two(entries):
+    valid_count = 0
+
+    # For each entry
+    for e in entries:
+        # Check if valid
+        if bool(e[3][e[0]-1] == e[2]) != bool(e[3][e[1]-1] == e[2]):
+            # Increment valid count
+            valid_count = valid_count + 1
+
+    return valid_count
+
+if __name__ == "__main__":
+    # Get input from txt file
+    with open(os.getcwd() + '\\2020\\Day 2\\input.txt', 'r') as file_obj:
+        file_input = file_obj.readlines()
+    
+    # Clean input
+    entries = []
+    for entry in file_input:
+        tokens = entry.split(' ')
+        entries.append((
+            int(tokens[0].split('-')[0]),
+            int(tokens[0].split('-')[1]),
+            tokens[1][0],
+            tokens[2].rstrip()
+        ))
+    
+    # Part one
+    print(part_one(entries))
+
+    # Part two
+    print(part_two(entries))
