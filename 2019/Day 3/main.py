b32b88425c44fa6ec7af4ae1d1ad918acad61c57
Edward Gorman <ejgorman@gmail.com>
2021-02-27 00:24:51 +0000

    Day 3 Part 1 solution

---
+++ 2019/Day 3/main.py
@@ -0,0 +1,102 @@
+import os
+
+def part_one_helper(position, direction, value):
+    if direction == 'U':
+        return (position[0] - value, position[1])
+    if direction == 'R':
+        return (position[0], position[1] + value)
+    if direction == 'D':
+        return (position[0] + value, position[1])
+    if direction == 'L':
+        return (position[0], position[1] - value)
+
+def part_one(wire_paths):
+    # Find list of vertexes per wire
+    vertexes_list = []
+    
+    # For each wire's path
+    for path in wire_paths:
+        # Initialise wire
+        position = (0, 0)
+        vertexes = []
+
+        # For each direction in path
+        for direction, value in path:
+            # Calculate new position
+            position = part_one_helper(
+                position,
+                direction,
+                value
+            )
+
+            # Add line to this list
+            vertexes.append(position)
+
+        # Append lines to the list
+        vertexes_list.append(vertexes)
+    
+    # Find the crossing points of the wire
+    crossing_points = []
+
+    u_last = (0, 0)
+    # For each vertex in the first wire
+    for u in vertexes_list[0]:
+
+        v_last = (0, 0)
+        # For each vertex in the second wire
+        for v in vertexes_list[1]:
+            # If u is vertical
+            if u[0] == u_last[0]:
+                # If v is horizontal
+                if v[1] == v_last[1]:
+                    # Check v within y bounds of u
+                    if u[1] <= v[1] <= u_last[1] or u_last[1] <= v[1] <= u[1]:
+                        # Check u within x bounds of v
+                        if v[0] <= u[0] <= v_last[0] or v_last[0] <= u[0] <= v[0]:
+                            # Crossing at ux, vy
+                            crossing_points.append((u[0], v[1]))
+
+            # Else u is horizontal
+            else:
+                # If v is vertical
+                if v[0] == v_last[0]:
+                    # Check v within x bounds of u
+                    if u[0] <= v[0] <= u_last[0] or u_last[0] <= v[0] <= u[0]:
+                        # Check u within y bounds of v
+                        if v[1] <= u[1] <= v_last[1] or v_last[1] <= u[1] <= v[1]:
+                            # Crossing at vx, uy
+                            crossing_points.append((v[0], u[1]))
+            
+            v_last = v
+        u_last = u
+
+    # Find shortest crossing point from origin using manhatten distance
+    crossing_distances = sorted([abs(x) + abs(y) for x, y in crossing_points])
+
+    if crossing_distances[0] == 0:
+        return crossing_distances[1]
+    else:
+        return crossing_distances[0]
+
+def part_two(entries):
+    pass
+
+if __name__ == "__main__":
+    # Get input from txt file
+    with open(os.getcwd() + '\\2019\\Day 3\\input.txt', 'r') as file_obj:
+        file_input = file_obj.readlines()
+    
+    # Clean input
+    entries = []
+    for entry in file_input:
+        moves = []
+        for move in entry.rstrip().split(','):
+            moves.append((move[0], int(move[1:])))
+
+        entries.append(moves)
+    
+    # Part one
+    print(part_one(entries))
+
+    # Part two
+    print(part_two(entries))
