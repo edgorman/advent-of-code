7edc15c5d09b7a24446f851f37dcb26e320ce810
Edward Gorman <ejgorman@gmail.com>
2021-12-01 14:36:00 +0000

    Day 1 solution

---
+++ Day X/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from txt file
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            // Clean input
+            int[] input = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                input[i] = int.Parse(lines[i]);
+            }
+
+            // Part 1
+            Console.WriteLine(PartOne(input));
+
+            // Part 2
+            Console.WriteLine(PartTwo(input));
+        }
+
+        static string PartOne(int[] input)
+        {
+            return "";
+        }
+
+        static string PartTwo(int[] input)
+        {
+            return "";
+        }
+    }
+}
