d460129954ab1f271fee814fab9658017c536c7e
Edward Gorman <ejgorman@gmail.com>
2022-02-13 22:21:10 +0000

    Day 25 Cleaned up

--- 2021/Day 25/Day 25 - CS/Program.cs
+++ 2021/Day 25/Day 25 - CS/Program.cs
@@ -8,24 +8,15 @@ namespace AdventOfCode2021
             // Get input from txt file
             string[] lines = System.IO.File.ReadAllLines("input.txt");
 
-            // Part 1
-            Console.WriteLine(PartOne(lines));
-
-            // Part 2
-            Console.WriteLine(PartTwo(lines));
-        }
-
-        static string PartOne(string[] seafloor)
-        {
             // Extract east and south facing cucumbers from seafloor
+            var seafloorShape = Tuple.Create(lines[0].Length, lines.Length);
             var eastCucumbers = new HashSet<Tuple<int, int>>();
             var southCucumbers = new HashSet<Tuple<int, int>>();
-
-            for (int y = 0; y < seafloor.Length; y++)
+            for (int y = 0; y < seafloorShape.Item2; y++)
             {
-                for (int x = 0; x < seafloor[y].Length; x++)
+                for (int x = 0; x < seafloorShape.Item1; x++)
                 {
-                    char value = seafloor[y][x];
+                    char value = lines[y][x];
 
                     if (value.Equals('>'))
                     {
@@ -38,51 +29,79 @@ namespace AdventOfCode2021
                 }
             }
 
+            // Part 1
+            Console.WriteLine(PartOne(eastCucumbers, southCucumbers, seafloorShape));
+
+            // Part 2
+            Console.WriteLine(PartTwo(eastCucumbers, southCucumbers, seafloorShape));
+        }
+
+        static string PartOne(
+            HashSet<Tuple<int, int>> eastCucumbers, 
+            HashSet<Tuple<int, int>> southCucumbers,
+            Tuple<int, int> seafloorShape
+        )
+        {
             // Keep count of current step and cucumbers moving this step
             var stepCount = 0;
-            var movingCount = int.MaxValue;
+            var hasMoved = true;
 
-            // Iterate until no cucumbers can move
-            while (movingCount > 0)
+            // Iterate until no cucumbers have moved
+            while (hasMoved)
             {
-                movingCount = 0;
+                // Set moved to false and initialise empty set of new north cucumbers
+                hasMoved = false;
                 var newEastCucumbers = new HashSet<Tuple<int, int>>();
-                var newSouthCucumbers = new HashSet<Tuple<int, int>>();
 
                 // Move east facing cucumbers first
                 foreach (var c in eastCucumbers)
                 {
+                    // Calculate new position
+                    var newX = (c.Item1 + 1) % seafloorShape.Item1;
+                    var newY = c.Item2;
+                    var newPosition = Tuple.Create(newX, newY);
+
                     // If there is no cucumber in the position ahead
-                    var newPosition = Tuple.Create((c.Item1 + 1) % seafloor[0].Length, c.Item2);
                     if (!eastCucumbers.Contains(newPosition) && !southCucumbers.Contains(newPosition))
                     {
+                        // Move cucumber to new position
                         newEastCucumbers.Add(newPosition);
-                        movingCount++;
+                        hasMoved = true;
                     }
                     else
                     {
+                        // Keep cucumber at same position
                         newEastCucumbers.Add(c);
                     }
                 }
 
-                // Move east facing cucumbers first
+                // Update east cucumbers and initialise empty set of new south cucumbers
+                eastCucumbers = newEastCucumbers;
+                var newSouthCucumbers = new HashSet<Tuple<int, int>>();
+
+                // Move south facing cucumbers second
                 foreach (var c in southCucumbers)
                 {
+                    // Calculate new position
+                    var newX = c.Item1;
+                    var newY = (c.Item2 + 1) % seafloorShape.Item2;
+                    var newPosition = Tuple.Create(newX, newY);
+
                     // If there is no cucumber in the position ahead
-                    var newPosition = Tuple.Create(c.Item1, (c.Item2 + 1) % seafloor.Length);
                     if (!newEastCucumbers.Contains(newPosition) && !southCucumbers.Contains(newPosition))
                     {
+                        // Move cucumber to new position
                         newSouthCucumbers.Add(newPosition);
-                        movingCount++;
+                        hasMoved = true;
                     }
                     else
                     {
+                        // Keep cucumber at same position
                         newSouthCucumbers.Add(c);
                     }
                 }
 
-                // Update cucumbers and increment step count
-                eastCucumbers = newEastCucumbers;
+                // Update south cucumbers and increment step count
                 southCucumbers = newSouthCucumbers;
                 stepCount++;
             }
@@ -90,7 +109,11 @@ namespace AdventOfCode2021
             return stepCount.ToString();
         }
 
-        static string PartTwo(string[] input)
+        static string PartTwo(
+            HashSet<Tuple<int, int>> eastCucumbers,
+            HashSet<Tuple<int, int>> southCucumbers,
+            Tuple<int, int> seafloorShape
+        )
         {
             return "";
         }
